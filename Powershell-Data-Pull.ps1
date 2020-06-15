638a6c77f7f025fb72d65091a9e28ab8ff989625
Aaron Rosenmund <aaron.rosenmund@gmail.com>
2020-06-15 07:50:40 -0400

    updated powershell doc
    you have to change the file to point at your file and dump into your location but it works and is in a more readable format.

--- Powershell-Data-Pull.ps1
+++ Powershell-Data-Pull.ps1
@@ -3,12 +3,13 @@
 
 # Import log files
 
-$firewall_raw = get-content .\aaron-box-pfirewall.log
+$firewall_raw = get-content D:\ELK-IR\aaron-box-pfirewall.log
 
 $i = $firewall_raw.count
 $fw = $firewall_raw[5..$i]
 
 $fw_obj = @()
+$nodns = "null"
 
 foreach($f in $fw){
 
@@ -19,11 +20,13 @@ $properties = @{
     Computer_Name = $env:COMPUTERNAME
     Domain_Name = $env:USERDOMAIN
     #can split these later but elastic search seamed to pull this type okay.
-    date_time = $l[0,1]
+    date_time = [string]$l[0,1]
     action = $l[2]
     protocol = $l[3]
     srcip = $l[4]
+    #srcdns = try{[string](resolve-dnsName $l[4] -ErrorAction SilentlyContinue).NameHost}catch{$nodns}finally{}
     dstip = $l[5]
+    #dstdns = try{[string](resolve-dnsName $l[5] -ErrorAction SilentlyContinue).NameHost}catch{$nodns}finally{}
     srcport = $l[6]
     dstport = $l[7]
     size = $l[8]
@@ -44,11 +47,12 @@ $fw_obj += $obj
 
 }
 
-$fw_obj |convertto-csv 
+$date = get-date -Format MM-dd-yy
+
+$fw_obj | export-csv -NoTypeInformation -Encoding UTF8 -Delimiter "," d:/ELK-IR/veronica-pfirewall-$date.csv
 
 
 #resolve dns in logstash
-
 #overlay sites in geo tiles
 
 
